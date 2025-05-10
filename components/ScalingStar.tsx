92abf3cf32283e1b8439b6feaec61b50d31d6a0f
Berna İkiz <34381430+berna-ikiz@users.noreply.github.com>
2025-05-11 02:02:38 +0300

    Add animated scaling to change the animation size with color transition

--- components/ScalingStar.tsx
+++ components/ScalingStar.tsx
@@ -4,27 +4,43 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 const ScalingStar = () => {
   const animationStar = useRef(new Animated.Value(0)).current;
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     Animated.loop(
-        Animated.timing(animationStar,{
-            toValue:1,
-            duration:3000,
-            useNativeDriver: false,
-        })
+      Animated.sequence([
+        Animated.timing(animationStar, {
+          toValue: 1,
+          duration: 3000,
+          useNativeDriver: false,
+        }),
+        Animated.timing(animationStar, {
+          toValue: 0,
+          duration: 2000,
+          useNativeDriver: false,
+        }),
+      ])
     ).start();
-  },[])
+  }, []);
   const rotate = animationStar.interpolate({
-    inputRange:[0,1],
-    outputRange:["0deg","360deg"]
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
   });
 
   const backgroundColor = animationStar.interpolate({
-    inputRange:[0, 0.5,1],
-    outputRange: ["darkblue","lightblue","darkblue"]
-  })
+    inputRange: [0, 0.5, 1],
+    outputRange: ["darkblue", "lightblue", "darkblue"],
+  });
+  const scale = animationStar.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 2],
+  });
   return (
-    <Animated.View style={[styles.starContainer,{transform:[{rotate}],backgroundColor}]}>
+    <Animated.View
+      style={[
+        styles.starContainer,
+        { transform: [{ rotate }, { scale }], backgroundColor },
+      ]}
+    >
       <FontAwesome name="star" size={200} color="gold" />
     </Animated.View>
   );
