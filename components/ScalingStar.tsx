c4c770b0612e4fc5fe3d5a45de34d02323bb44f2
Berna İkiz <34381430+berna-ikiz@users.noreply.github.com>
2025-05-11 01:48:56 +0300

    rotate has been added

--- components/ScalingStar.tsx
+++ components/ScalingStar.tsx
@@ -1,23 +1,39 @@
-import { View, Text, Animated, StyleSheet } from 'react-native'
-import React from 'react'
-import { FontAwesome } from '@expo/vector-icons'
+import { Animated, StyleSheet } from "react-native";
+import React, { useEffect, useRef } from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 const ScalingStar = () => {
-  return (
-    <Animated.View style ={styles.starContainer}>
-        <Text>aaa</Text>
-        <FontAwesome name='star' size={50} color="white" />
-    </Animated.View>
-  )
-}
-const styles = StyleSheet.create({
-    starContainer : {
-        width: 100,
-        height: 100,
-        borderRadius:50,
-        justifyContent:"center",
-        alignItems:"center"
-    }
-})
+  const animationStar = useRef(new Animated.Value(0)).current;
+  
+  useEffect(()=>{
+    Animated.loop(
+        Animated.timing(animationStar,{
+            toValue:1,
+            duration:3000,
+            useNativeDriver: false,
+        })
+    ).start();
+  },[])
+  const rotate = animationStar.interpolate({
+    inputRange:[0,1],
+    outputRange:["0deg","360deg"]
+  });
 
-export default ScalingStar
+
+  return (
+    <Animated.View style={[styles.starContainer,{transform:[{rotate}]}]}>
+      <FontAwesome name="star" size={200} color="gold" />
+    </Animated.View>
+  );
+};
+const styles = StyleSheet.create({
+  starContainer: {
+    width: 200,
+    height: 200,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default ScalingStar;
